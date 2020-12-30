c009c71fd59b8117fbb0a88c1d55cfc93f6b6e3e
Wiktor <wykwit@disroot.org>
2020-12-30 14:29:35 +0100

    Update pawn movement

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -23,27 +23,43 @@ public class HumanPlayer implements Player {
     }
 
     void consecutiveMove(Field f) {
-        // TODO: only allow consecutive moves if the kill is possible
         Field t;
         board.display();
         while (true) {
             System.out.print("to> ");
             t = inputToField(scanner.nextLine());
             if (t == f) break;
+            if (!board.possibleKills(f).contains(t)) {
+                System.out.println("Invalid move. You have to kill!");
+            }
             int move = board.movePawn(f, t);
             if (move < 0)
                 System.out.println("Invalid move. Try again...");
-            else if (move > 0)
+            else if (move > 0 && !board.possibleKills(t, f).isEmpty())
                 f = t;
             else
                 break;
         }
     }
 
+    private boolean anyKillPossible() {
+        for (int x = 0; x < board.size; x++) {
+            for (int y = 0; y < board.size; y++) {
+                Field p = board.getField(x, y);
+                if (p.getColor() == color && !board.possibleKills(p).isEmpty())
+                    return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void nextMove() {
         Field f, t;
         board.display();
+        if (anyKillPossible()) {
+            System.out.println("Killing this turn is possible.");
+        }
         while (true) {
             System.out.print("from> ");
             f = inputToField(scanner.nextLine());
@@ -56,7 +72,7 @@ public class HumanPlayer implements Player {
             int move = board.movePawn(f, t);
             if (move < 0)
                 System.out.println("Invalid move. Try again...");
-            else if (move > 0) {
+            else if (move > 0 && !board.possibleKills(t, f).isEmpty()) {
                 consecutiveMove(t);
                 break;
             } else
