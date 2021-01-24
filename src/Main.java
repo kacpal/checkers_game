c9d839caba3213f79d5dad6283345db542c1e5a2
Wiktor <wykwit@disroot.org>
2021-01-24 01:55:40 +0100

    Refactor Main class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+        boolean playerGoesFirst = (args.length > 0 && args[0].equals("--first"));
+
         UI ui = new ConsoleUI();
-        Board b = new Board(ui);
-        Player p1 = new ComputerPlayer(b, 1);
-        Player p2 = new HumanPlayer(b, 2);
-        while (b.gameState.gameover() == 0) {
-                b.display();
-                p1.nextMove();
-                b.display();
-                p2.nextMove();
+        Board board = new Board(ui);
+
+        Player a, b;
+
+        if (playerGoesFirst) {
+            a = new HumanPlayer(board, 1, ui);
+            b = new ComputerPlayer(board, 2);
+        } else {
+            a = new ComputerPlayer(board, 1);
+            b = new HumanPlayer(board, 2, ui);
         }
-        b.display();
-        ui.displayResults(b.gameState.gameover());
+
+        ui.displayHello();
+
+        // main game loop
+        while (board.gameState.gameover() == 0) {
+                board.display();
+                a.nextMove();
+                board.display();
+                b.nextMove();
+        }
+
+        board.display();
+        ui.displayResults(board.gameState.gameover());
+
+        return;
     }
 }
